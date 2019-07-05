94a99bd55223718d837b4bd05cab3179e2d89a64
JohnReid <johnbaronreid@netscape.net>
2019-07-05 15:36:02 +0100

    Add temperature scaling baseline.

---
+++ baselines/diabetic_retinopathy_diagnosis/temperature_scaling/main.py
@@ -0,0 +1,214 @@
+# Copyright 2019 BDL Benchmarks Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Script for training and evaluating temperature scaling baseline for
+Diabetic Retinopathy Diagnosis benchmark."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import functools
+import datetime
+
+import bdlb
+# from bdlb.core import plotting
+from baselines.diabetic_retinopathy_diagnosis.temperature_scaling import model
+
+from absl import app
+from absl import flags
+import tensorflow as tf
+tf.__version__
+tfk = tf.keras
+tfkm = tfk.models
+
+bdlb.tf_limit_memory_growth()
+
+##########################
+# Command line arguments #
+##########################
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    name="output_dir",
+    default=os.path.join('/tmp', 'BDLB'),
+    help="Path to store model, tensorboard and report outputs.",
+)
+flags.DEFINE_enum(
+    name="level",
+    default="medium",
+    enum_values=["realworld", "medium"],
+    help="Downstream task level, one of {'medium', 'realworld'}.",
+)
+flags.DEFINE_integer(
+    name="batch_size",
+    default=128,
+    help="Batch size used for training.",
+)
+flags.DEFINE_integer(
+    name="max_batches",
+    default=0,
+    help="Maximum number of batches used for training in each epoch.",
+)
+flags.DEFINE_integer(
+    name="num_epochs",
+    default=50,
+    help="Number of epochs of training over the whole training set.",
+)
+flags.DEFINE_integer(
+    name="num_mc_samples",
+    default=10,
+    help="Number of Monte Carlo samples used for uncertainty estimation.",
+)
+flags.DEFINE_enum(
+    name="uncertainty",
+    default="entropy",
+    enum_values=["stddev", "entropy"],
+    help="Uncertainty type, one of those defined "
+    "with `estimator` function.",
+)
+flags.DEFINE_integer(
+    name="num_base_filters",
+    default=32,
+    help="Number of base filters in convolutional layers.",
+)
+flags.DEFINE_float(
+    name="learning_rate",
+    default=4e-4,
+    help="ADAM optimizer learning rate.",
+)
+flags.DEFINE_float(
+    name="dropout_rate",
+    default=0.1,
+    help="The rate of dropout, between [0.0, 1.0).",
+)
+flags.DEFINE_float(
+    name="l2_reg",
+    default=5e-5,
+    help="The L2-regularization coefficient.",
+)
+
+
+def main(argv):
+
+  print(argv)
+  print(FLAGS)
+
+  current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  out_dir = os.path.join(FLAGS.output_dir, 'TS', current_time)
+
+  ##########################
+  # Hyperparmeters & Model #
+  ##########################
+  input_shape = dict(medium=(256, 256, 3), realworld=(512, 512, 3))[FLAGS.level]
+
+  hparams = dict(dropout_rate=FLAGS.dropout_rate,
+                 num_base_filters=FLAGS.num_base_filters,
+                 learning_rate=FLAGS.learning_rate,
+                 l2_reg=FLAGS.l2_reg,
+                 input_shape=input_shape)
+  classifier = model.VGG(**hparams)
+  classifier.summary()
+  print('********** Output dir: {} ************'.format(out_dir))
+
+  CHKPT_DIR = '/tmp/BDLB/MCdropout/20190705-091825/checkpoints'
+  latest = tf.train.latest_checkpoint(CHKPT_DIR)
+  print('********** Loading checkpoint weights: {}'.format(latest))
+  classifier.load_weights(latest)
+
+  #############
+  # Load Task #
+  #############
+  dtask = bdlb.load(
+      benchmark="diabetic_retinopathy_diagnosis",
+      level=FLAGS.level,
+      batch_size=FLAGS.batch_size,
+      download_and_prepare=False,  # do not download data from this script
+  )
+  ds_train, ds_validation, ds_test = dtask.datasets
+
+  #################
+  # Training Loop #
+  #################
+  # current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  # if FLAGS.max_batches > 0:
+  #   train_data = ds_train.take(FLAGS.max_batches)
+  # else:
+  #   train_data = ds_train
+  # history = classifier.fit(
+  #     train_data,
+  #     epochs=FLAGS.num_epochs,
+  #     validation_data=ds_validation,
+  #     class_weight=dtask.class_weight(),
+  #     callbacks=[
+  #         tfk.callbacks.TensorBoard(
+  #             log_dir=os.path.join(out_dir, 'fit'),
+  #             update_freq="epoch",
+  #             write_graph=True,
+  #             histogram_freq=1,
+  #         ),
+  #         tfk.callbacks.ModelCheckpoint(
+  #             filepath=os.path.join(out_dir, "checkpoints", "weights-{epoch}.ckpt"),
+  #             verbose=1,
+  #             save_weights_only=True,
+  #         )
+  #     ],
+  # )
+  # plotting.tfk_history(history, output_dir=os.path.join(out_dir, "history"))
+
+  ########################
+  # Optimise temperature #
+  ########################
+  ts_layer = model.TemperatureScaling()
+  ts_model = tfkm.Sequential([classifier, model.BinaryProbToMulticlass(), ts_layer])
+  #
+  # Calculate logits for test set
+  y_true = []
+  logits = []
+  for x, y in ds_validation:
+    p = classifier(x)
+    logit = tf.math.log(p) - tf.math.log(1 - p)
+    logit_multi = tf.concat([tf.zeros_like(logit), logit], axis=1)
+    logits.append(logit_multi)
+    y_true.append(tf.one_hot(y, depth=2))
+  y_true = tf.concat(y_true, axis=0)
+  logits = tf.concat(logits, axis=0)
+  #
+  # Optimise temperature
+  ts_layer.optimise_temperature(y_true, logits)
+
+  ##############
+  # Evaluation #
+  ##############
+  dtask.evaluate(functools.partial(model.predict,
+                                   model=ts_model,
+                                   type=FLAGS.uncertainty),
+                 dataset=ds_test,
+                 output_dir=os.path.join(out_dir, 'evaluation'))
+  print('Temperature: ', ts_layer.temperature.value)
+
+  # Check beta distribution
+  # import numpy as np
+  # import scipy.stats as stats
+  # import pylab as pl
+  # p = np.linspace(0, 1, num=101)
+  # pl.plot(p, stats.beta.pdf(p, a=2, b=5 / 4), '-o')
+  # pl.savefig('beta.png')
+  # # !eog beta.png
+
+
+if __name__ == "__main__":
+  import sys
+  sys.argv = sys.argv[:1]
+  app.run(main)
---
+++ baselines/diabetic_retinopathy_diagnosis/temperature_scaling/model.py
@@ -0,0 +1,303 @@
+# Copyright 2019 BDL Benchmarks Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Model definition of the VGGish network for temperature scaling baseline."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from bdlb.diabetic_retinopathy_diagnosis.benchmark import DiabeticRetinopathyDiagnosisBenchmark
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfk = tf.keras
+tfkl = tfk.layers
+
+
+def VGG(dropout_rate, num_base_filters, learning_rate, l2_reg, input_shape):
+  """VGG-like model for diabetic retinopathy diagnosis.
+
+  Args:
+    dropout_rate: `float`, the rate of dropout, between [0.0, 1.0).
+    num_base_filters: `int`, number of convolution filters in the
+      first layer.
+    learning_rate: `float`, ADAM optimizer learning rate.
+    l2_reg: `float`, the L2-regularization coefficient.
+    input_shape: `iterable`, the shape of the images in the input layer.
+
+  Returns:
+    A tensorflow.keras.Sequential VGG-like model with dropout.
+  """
+
+  # Feedforward neural network
+  model = tfk.Sequential([
+      tfkl.InputLayer(input_shape),
+      # Block 1
+      tfkl.Conv2D(filters=num_base_filters,
+                  kernel_size=3,
+                  strides=(2, 2),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.MaxPooling2D(pool_size=3, strides=(2, 2), padding="same"),
+      # Block 2
+      tfkl.Conv2D(filters=num_base_filters,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.MaxPooling2D(pool_size=3, strides=(2, 2), padding="same"),
+      # Block 3
+      tfkl.Conv2D(filters=num_base_filters * 2,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 2,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.MaxPooling2D(pool_size=3, strides=(2, 2), padding="same"),
+      # Block 4
+      tfkl.Conv2D(filters=num_base_filters * 4,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 4,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 4,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 4,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.MaxPooling2D(pool_size=3, strides=(2, 2), padding="same"),
+      # Block 5
+      tfkl.Conv2D(filters=num_base_filters * 8,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 8,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 8,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      tfkl.Dropout(dropout_rate),
+      tfkl.Conv2D(filters=num_base_filters * 8,
+                  kernel_size=3,
+                  strides=(1, 1),
+                  padding="same",
+                  kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("relu"),
+      # Global poolings
+      tfkl.Lambda(lambda x: tfk.backend.concatenate(
+          [tfkl.GlobalAvgPool2D()(x),
+           tfkl.GlobalMaxPool2D()(x)], axis=1)),
+      # Fully-connected
+      tfkl.Dense(1, kernel_regularizer=tfk.regularizers.l2(l2_reg)),
+      tfkl.Activation("sigmoid")
+  ])
+
+  model.compile(loss=DiabeticRetinopathyDiagnosisBenchmark.loss(),
+                optimizer=tfk.optimizers.Adam(learning_rate),
+                metrics=DiabeticRetinopathyDiagnosisBenchmark.metrics())
+
+  return model
+
+
+def predict(x, model, type="entropy"):
+  """Temperature scaled uncertainty estimator.
+
+  Args:
+    x: `numpy.ndarray`, datapoints from input space,
+      with shape [B, H, W, 3], where B the batch size and
+      H, W the input images height and width accordingly.
+    model: `tensorflow.keras.Model`, a probabilistic model,
+      which accepts input with shape [B, H, W, 3] and
+      outputs logits with shape [B, 2].
+    type: (optional) `str`, type of uncertainty returns,
+      one of {"entropy", "stddev"}.
+
+  Returns:
+    mean: `numpy.ndarray`, predictive mean, with shape [B].
+    uncertainty: `numpy.ndarray`, uncertainty in prediction,
+      with shape [B].
+  """
+  from scipy.stats import bernoulli
+  #
+  logits = model(x, training=False)
+  p = tf.nn.softmax(logits)[:, 1]
+  #
+  # Bernoulli output distribution
+  dist = bernoulli(p)
+  #
+  # Predictive mean calculation
+  mean = dist.mean()
+  #
+  # Choose uncertainty measure
+  if type == "entropy":
+    # Use predictive entropy for uncertainty
+    uncertainty = dist.entropy()
+  elif type == "stddev":
+    # Use predictive standard deviation for uncertainty
+    uncertainty = dist.std()
+  else:
+    raise ValueError(
+        "Unrecognized type={} provided, use one of {'entropy', 'stddev'}".
+        format(type))
+  #
+  return mean, uncertainty
+
+
+class BinaryProbToMulticlass(tfkl.Layer):
+  """
+  A Keras layer that takes binary classification probabilities and converts them to
+  2-way multiclass probabilities.
+  """
+
+  def __init__(self):
+    super(BinaryProbToMulticlass, self).__init__()
+
+  def build(self, input_shape):
+    super(BinaryProbToMulticlass, self).build(input_shape)  # Be sure to call this at the end
+
+  def compute_output_shape(self, input_shape):
+    print('input_shape: ', input_shape)
+    return input_shape + (2,)
+
+  def call(self, p_pos):
+    p_neg = 1 - p_pos
+    result = tf.concat([p_neg, p_pos], axis=-1, name='multiclass')
+    # print('p_pos: ', p_pos.shape)
+    # print('result: ', result.shape)
+    return result
+
+
+class TemperatureScaling(tfkl.Layer):
+  """
+  A Keras layer that wraps a model with temperature scaling.
+  Note that the inputs should be the classification logits, not the softmax or log softmax.
+  """
+
+  def __init__(self):
+    super(TemperatureScaling, self).__init__()
+    self.temperature = tf.Variable(initial_value=[1.5])
+    # print('temperature: ', self.temperature.shape)
+
+  def build(self, input_shape):
+    super(TemperatureScaling, self).build(input_shape)  # Be sure to call this at the end
+
+  def compute_output_shape(self, input_shape):
+    return input_shape
+
+  def call(self, logits):
+    """
+    Perform temperature scaling on logits
+    """
+    return tf.divide(logits, self.temperature, name='scaled')
+
+  def create_objective(self, y_true, logits):
+    """
+    Create an objective function to use to optimise the temperature.
+
+    Args:
+      y_true: the ground truth labels
+      logits: the logits
+
+    Returns:
+      An objective function that returns (NLL, dNLL / dTemp) for a given temperature
+    """
+    def objective(temperature):
+      self.temperature.assign(temperature)
+      #
+      # Don't calculate gradients for any variables except for temperature
+      with tf.GradientTape(watch_accessed_variables=False) as tape:
+        tape.watch(self.temperature)
+        scaled_logits = self.call(logits)
+        loss_per_sample = tfk.losses.categorical_crossentropy(y_true, scaled_logits, from_logits=True)
+        loss = tf.reduce_sum(loss_per_sample, name='loss')
+      grads = tape.gradient(loss, self.temperature)
+      # print('loss: ', loss.shape)
+      # print('grads: ', grads.shape)
+      return loss, grads
+
+    return objective
+
+  def optimise_temperature(self, y_true, logits):
+    """
+    Tune the temperature of the model (using the validation set).
+    We're going to set it to optimize NLL.
+    valid_loader (DataLoader): validation set loader
+    """
+    #
+    # Create the objective function
+    objective = self.create_objective(y_true, logits)
+    #
+    # Run the optimisation
+    results = tfp.optimizer.bfgs_minimize(objective, self.temperature, max_iterations=50)
+    # print(dir(results))
+    #
+    # Check the results
+    if not results.converged:
+      import warnings
+      warnings.warn('LBFGS did not converge')
+      print(results)
+    #
+    # Update the temperature
+    print('Optimised temperature: {}'.format(results.position))
+    self.temperature.assign(results.position)
+    #
+    return self
