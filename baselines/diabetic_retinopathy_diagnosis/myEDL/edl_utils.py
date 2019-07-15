corrected num_samples in deterministic models and also corrected the prediction class for the calculation of uncertainty.

baselines/diabetic_retinopathy_diagnosis/myEDL/edl_utils.py
@@ -17,6 +17,14 @@ def exp_evidence(logits, clip_value=10.0):
   """Calculate evidence from logits using a clipped exp."""
   return tf.exp(logits/10, name='exp_evidence')
 
+def get_pub(logits, logits2evidence=exp_evidence):
+    ev = logits2evidence(logits)
+    alpha = ev + 1
+    b = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
+    p = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
+    u = 2.0 / tf.reduce_sum(alpha, axis=1, keepdims=True)
+    return p, u, b
+
 
 def KL(alpha):
   """Calculate the Kullback-Leibler divergence of a Dirichlet(alpha)
@@ -73,12 +81,6 @@ def loss():
       y_hot = tf.one_hot(tf.cast(y[:,0],tf.int32),depth=2)
       return mse_loss(y_hot, alpha)
   return my_loss
-
-def loss1():
-  def my_loss(y, logits):
-      return tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y)
-  return my_loss
-
 
 def categorical_accuracy(y_true, y_pred):
     if len(y_true.get_shape().as_list()) > 1:

baselines/diabetic_retinopathy_diagnosis/myEDL/model_myedl.py
@@ -19,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 import tensorflow as tf
 
-from baselines.diabetic_retinopathy_diagnosis.myEDL.edl_utils import loss, metrics
+from baselines.diabetic_retinopathy_diagnosis.myEDL.edl_utils import loss, metrics, get_pub
 
 
 def VGGDrop(dropout_rate, num_base_filters, learning_rate, l2_reg, input_shape):
@@ -151,17 +151,19 @@ def VGGDrop(dropout_rate, num_base_filters, learning_rate, l2_reg, input_shape):
     tfkl.Dense(2, kernel_regularizer=tfk.regularizers.l2(l2_reg)),
     #tfkl.Activation("sigmoid")
   ])
-  model.global_step = tf.Variable(tf.constant(0), trainable=False, name='global_step')
 
+  optimizer = tfk.optimizers.Adam(learning_rate=learning_rate)
+
+  model.load_weights("/tmp/checkpoints/weights-50.ckpt")
 
   model.compile(loss= loss(),
-                optimizer=tfk.optimizers.Adam(learning_rate),
+                optimizer=optimizer,
                 metrics=metrics())
   DiabeticRetinopathyDiagnosisBenchmark.metrics()
 
   return model
 
-def predict(x, model, num_samples, type="entropy"):
+def predict(x, model, type="entropy"):
   """Simple sigmoid uncertainty estimator.
     
   Args:
@@ -188,10 +190,12 @@ def predict(x, model, num_samples, type="entropy"):
   B, _, _, _ = x.shape
 
   # Single forward pass from the deterministic model
-  p = model(x, training=False)
+  logits = model(x, training=False)
+  p,u,b = get_pub(logits=logits)
+
 
   # Bernoulli output distribution
-  dist = scipy.stats.bernoulli(p)
+  dist = scipy.stats.bernoulli(p[:,1])
 
   # Predictive mean calculation
   mean = dist.mean()