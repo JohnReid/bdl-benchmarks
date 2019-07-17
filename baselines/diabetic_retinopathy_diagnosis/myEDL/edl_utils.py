corrected num_samples in deterministic models and also corrected the prediction class for the calculation of uncertainty.

baselines/diabetic_retinopathy_diagnosis/myEDL/edl_utils.py
@@ -1,29 +1,29 @@
 import tensorflow as tf
 import numpy as np
 
-def annealing_coefficient(epoch):
-  """The annealing coefficient grows as the number of epochs to a maximum of 1.
-  """
-  coef = tf.minimum(1.0, tf.cast(epoch, tf.float32) / 10)
-  return coef
-
 
 def relu_evidence(logits):
   """Calculate evidence from logits using a ReLU."""
   return tf.nn.relu(logits)
 
 
-def exp_evidence(logits, clip_value=10.0):
-  """Calculate evidence from logits using a clipped exp."""
-  return tf.exp(logits/10, name='exp_evidence')
+def exp_evidence(logits, smooth=10.0):
+  """Calculate evidence from logits using exp."""
+  return tf.exp(logits/smooth)
+
+
+def softplus_evidence(logits):
+  """Calculate evidence from logits using softplus."""
+  return tf.nn.softplus(logits)
 
 def get_pub(logits, logits2evidence=exp_evidence):
     ev = logits2evidence(logits)
     alpha = ev + 1
-    b = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
-    p = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
-    u = 2.0 / tf.reduce_sum(alpha, axis=1, keepdims=True)
-    return p, u, b
+    tot_alpha = tf.reduce_sum(alpha, axis=1, keepdims=True)
+    b = ev / tot_alpha
+    p = alpha / tot_alpha
+    u = 2.0 / tot_alpha
+    return alpha, p, u, b
 
 
 def KL(alpha):
@@ -43,7 +43,7 @@ def KL(alpha):
   kl = tf.reduce_sum((alpha - beta) * (dg1 - dg0), axis=1, keepdims=True) + lnB + lnB_uni
   return tf.reduce_mean(kl)
 
-def mse_loss(y, alpha):
+def mse_loss(y, alpha, annealing_coef=0.1):
     E = alpha - 1
     S = tf.reduce_sum(alpha, axis=1, keepdims=True)
     m = alpha / S
@@ -51,21 +51,17 @@ def mse_loss(y, alpha):
     A = tf.reduce_sum((y - m) ** 2, axis=1, keepdims=True)
     B = tf.reduce_sum(alpha * (S - alpha) / (S * S * (S + 1)), axis=1, keepdims=True)
 
-    annealing_coef = 0.1 #annealing_coefficient(epoch)
-
     alp = E * (1 - y) + 1
     C = annealing_coef * KL(alp)
     return (A + B) + C
 
 def EDL_loss(func=tf.math.digamma):
   """Evidential deep learning loss."""
-  def loss_func(y, alpha):
+  def loss_func(y, alpha, annealing_coef=0.1):
     S = tf.reduce_sum(alpha, axis=1, keepdims=True)
     E = alpha - 1
 
     A = tf.reduce_sum(y * (func(S) - func(alpha)), 1, keepdims=True)
-
-    annealing_coef = 0.1#annealing_coefficient(epoch)
 
     alp = E * (1 - y) + 1
     B = annealing_coef * KL(alp)
@@ -74,12 +70,14 @@ def EDL_loss(func=tf.math.digamma):
   return loss_func
 
 
-def loss():
+def loss(global_step=5000, annealing_step=50000):
   def my_loss(y, logits):
-      evidence = exp_evidence(logits)
-      alpha = evidence + 1
+      alpha, p, u, b = get_pub(logits)
       y_hot = tf.one_hot(tf.cast(y[:,0],tf.int32),depth=2)
-      return mse_loss(y_hot, alpha)
+
+      coef = 0.1#tf.minimum(1.0, tf.cast(global_step, tf.float32) / annealing_step)
+
+      return mse_loss(y_hot, alpha, coef)
   return my_loss
 
 def categorical_accuracy(y_true, y_pred):
@@ -88,20 +86,22 @@ def categorical_accuracy(y_true, y_pred):
     acc = tf.cast(tf.equal(y_true, tf.argmax(y_pred, axis=-1)), tf.float32)
     return acc
 
+def binary_accuracy(y_true, y_pred):
+    _, p,u,b=get_pub(y_pred)
+    y_pred = p[:, 1:2]
+    y_true = y_true[:, 0:1]
+    return tf.keras.metrics.BinaryAccuracy()(y_true, y_pred)
+
 # AUC for a binary classifier
 def auc_metric(y_true, y_pred):
-    ev = exp_evidence(y_pred)
-    alpha = ev + 1
-    p = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
+    _, p, u, b = get_pub(y_pred)
     y_pred = p[:, 1]
     y_true = y_true[:, 0]
     return tf.keras.metrics.AUC()(y_true, y_pred)
 
 def metrics():
     """Evaluation metrics used for monitoring training."""
-    import tensorflow as tf
-    tfk = tf.keras
-    return [categorical_accuracy, auc_metric] #[tfk.metrics.CategoricalAccuracy(), tfk.metrics.AUC()]
+    return [binary_accuracy, auc_metric]
 
 
 

baselines/diabetic_retinopathy_diagnosis/myEDL/model_myedl.py
@@ -152,9 +152,11 @@ def VGGDrop(dropout_rate, num_base_filters, learning_rate, l2_reg, input_shape):
     #tfkl.Activation("sigmoid")
   ])
 
+  global_step = tf.compat.v1.train.get_or_create_global_step()
   optimizer = tfk.optimizers.Adam(learning_rate=learning_rate)
+  optimizer.iterations = global_step
 
-  model.compile(loss= loss(),
+  model.compile(loss= loss(global_step),
                 optimizer=optimizer,
                 metrics=metrics())
   DiabeticRetinopathyDiagnosisBenchmark.metrics()
@@ -189,7 +191,7 @@ def predict(x, model, type="entropy"):
 
   # Single forward pass from the deterministic model
   logits = model(x, training=False)
-  p,u,b = get_pub(logits=logits)
+  _, p,u,b = get_pub(logits=logits)
 
 
   # Bernoulli output distribution