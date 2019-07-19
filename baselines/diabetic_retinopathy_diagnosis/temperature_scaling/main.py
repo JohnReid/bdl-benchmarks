Halfway through fixing temperature scaling.

baselines/diabetic_retinopathy_diagnosis/temperature_scaling/main.py
@@ -32,6 +32,7 @@ from absl import flags
 import tensorflow as tf
 tf.__version__
 tfk = tf.keras
+tfkl = tf.keras.layers
 tfkm = tfk.models
 
 bdlb.tf_limit_memory_growth()
@@ -137,49 +138,37 @@ def main(argv):
   )
   ds_train, ds_validation, ds_test = dtask.datasets
 
-  #################
-  # Training Loop #
-  #################
-  # current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-  # if FLAGS.max_batches > 0:
-  #   train_data = ds_train.take(FLAGS.max_batches)
-  # else:
-  #   train_data = ds_train
-  # history = classifier.fit(
-  #     train_data,
-  #     epochs=FLAGS.num_epochs,
-  #     validation_data=ds_validation,
-  #     class_weight=dtask.class_weight(),
-  #     callbacks=[
-  #         tfk.callbacks.TensorBoard(
-  #             log_dir=os.path.join(out_dir, 'fit'),
-  #             update_freq="epoch",
-  #             write_graph=True,
-  #             histogram_freq=1,
-  #         ),
-  #         tfk.callbacks.ModelCheckpoint(
-  #             filepath=os.path.join(out_dir, "checkpoints", "weights-{epoch}.ckpt"),
-  #             verbose=1,
-  #             save_weights_only=True,
-  #         )
-  #     ],
-  # )
-  # plotting.tfk_history(history, output_dir=os.path.join(out_dir, "history"))
+  ###############
+  # Build model #
+  ###############
+  binary_prob_to_multi = model.BinaryProbToMulticlass()
+  inv_softmax = model.InverseSoftmaxFixedMean(mean=1.)
+  ts_layer = model.TemperatureScaling()
+  multi_to_binary_prob = model.MulticlassToBinaryProb()
+  ts_model = tfkm.Sequential([classifier,
+                              binary_prob_to_multi,
+                              inv_softmax,
+                              ts_layer,
+                              tfkl.Softmax(),
+                              multi_to_binary_prob])
+  ts_model.build(input_shape=hparams['input_shape'])
+  classifier.summary()
+  ts_model.summary()
+  print('classifier: ', classifier.get_layer(1).output_shape)
+  print('ts_model: ', ts_model.get_layer(1).output_shape)
 
   ########################
   # Optimise temperature #
   ########################
-  ts_layer = model.TemperatureScaling()
-  ts_model = tfkm.Sequential([classifier, model.BinaryProbToMulticlass(), ts_layer])
   #
-  # Calculate logits for test set
+  # Calculate logits for validation set
   y_true = []
   logits = []
   for x, y in ds_validation:
     p = classifier(x)
-    logit = tf.math.log(p) - tf.math.log(1 - p)
-    logit_multi = tf.concat([tf.zeros_like(logit), logit], axis=1)
-    logits.append(logit_multi)
+    p_multi = binary_prob_to_multi(p)
+    logit = inv_softmax(p_multi)
+    logits.append(logit)
     y_true.append(tf.one_hot(y, depth=2))
   y_true = tf.concat(y_true, axis=0)
   logits = tf.concat(logits, axis=0)

baselines/diabetic_retinopathy_diagnosis/temperature_scaling/model.py
@@ -202,6 +202,50 @@ def predict(x, model, type="entropy"):
   return mean, uncertainty
 
 
+class InverseSoftmaxFixedMean(tfkl.Layer):
+  """
+  A reverse softmax layer that assumes the logits have a given mean.
+  """
+  def __init__(self, mean=0):
+    self.mean = mean
+    super(InverseSoftmaxFixedMean, self).__init__()
+
+  def build(self, input_shape):
+    super(InverseSoftmaxFixedMean, self).build(input_shape)
+
+  def compute_output_shape(self, input_shape):
+    return input_shape
+
+  def call(self, probs):
+    lp = tf.math.log(probs, name='log_probs')
+    lp_mean = tf.reduce_mean(lp, axis=-1, name='lp_mean')
+    adjustment = tf.subtract(lp_mean, tf.constant(self.mean, shape=lp_mean.shape, dtype=lp_mean.dtype),
+                             name='adjustment')
+    logits = tf.subtract(lp, tf.expand_dims(adjustment, axis=-1), name='logits')
+    return logits
+
+
+class MulticlassToBinaryProb(tfkl.Layer):
+  """
+  A Keras layer that takes 2-way classification probabilities and converts them to
+  probabilities of the positive class.
+  """
+
+  def __init__(self):
+    super(MulticlassToBinaryProb, self).__init__()
+
+  def build(self, input_shape):
+    super(MulticlassToBinaryProb, self).build(input_shape)
+
+  def compute_output_shape(self, input_shape):
+    return input_shape[:-1]
+
+  def call(self, probs):
+    result = tf.gather(probs, [1], axis=-1, name='multi_probs')
+    print('result: ', result.shape)
+    return result
+
+
 class BinaryProbToMulticlass(tfkl.Layer):
   """
   A Keras layer that takes binary classification probabilities and converts them to
@@ -212,10 +256,9 @@ class BinaryProbToMulticlass(tfkl.Layer):
     super(BinaryProbToMulticlass, self).__init__()
 
   def build(self, input_shape):
-    super(BinaryProbToMulticlass, self).build(input_shape)  # Be sure to call this at the end
+    super(BinaryProbToMulticlass, self).build(input_shape)
 
   def compute_output_shape(self, input_shape):
-    print('input_shape: ', input_shape)
     return input_shape + (2,)
 
   def call(self, p_pos):
@@ -224,6 +267,33 @@ class BinaryProbToMulticlass(tfkl.Layer):
     # print('p_pos: ', p_pos.shape)
     # print('result: ', result.shape)
     return result
+
+
+def create_temperature_scaling_objective(y_true, logits):
+  """
+  Create an objective function to use to optimise the temperature.
+
+  Args:
+    y_true: the ground truth labels
+    logits: the logits
+
+  Returns:
+    An objective function that returns (NLL, dNLL / dTemp) for a given temperature
+  """
+  def objective(temperature):
+    #
+    # Don't calculate gradients for any variables except for temperature
+    with tf.GradientTape(watch_accessed_variables=False) as tape:
+      tape.watch(temperature)
+      scaled_logits = tf.divide(logits, temperature, name='scaled')
+      loss_per_sample = tfk.losses.categorical_crossentropy(y_true, scaled_logits, from_logits=True)
+      loss = tf.reduce_sum(loss_per_sample, name='loss')
+    grads = tape.gradient(loss, temperature)
+    # print('loss: ', loss.shape)
+    # print('grads: ', grads.shape)
+    return loss, grads
+
+  return objective
 
 
 class TemperatureScaling(tfkl.Layer):
@@ -249,33 +319,6 @@ class TemperatureScaling(tfkl.Layer):
     """
     return tf.divide(logits, self.temperature, name='scaled')
 
-  def create_objective(self, y_true, logits):
-    """
-    Create an objective function to use to optimise the temperature.
-
-    Args:
-      y_true: the ground truth labels
-      logits: the logits
-
-    Returns:
-      An objective function that returns (NLL, dNLL / dTemp) for a given temperature
-    """
-    def objective(temperature):
-      self.temperature.assign(temperature)
-      #
-      # Don't calculate gradients for any variables except for temperature
-      with tf.GradientTape(watch_accessed_variables=False) as tape:
-        tape.watch(self.temperature)
-        scaled_logits = self.call(logits)
-        loss_per_sample = tfk.losses.categorical_crossentropy(y_true, scaled_logits, from_logits=True)
-        loss = tf.reduce_sum(loss_per_sample, name='loss')
-      grads = tape.gradient(loss, self.temperature)
-      # print('loss: ', loss.shape)
-      # print('grads: ', grads.shape)
-      return loss, grads
-
-    return objective
-
   def optimise_temperature(self, y_true, logits):
     """
     Tune the temperature of the model (using the validation set).
@@ -295,21 +338,26 @@ class TemperatureScaling(tfkl.Layer):
       warnings.warn('Could not import sail.metrics.')
     #
     # Create the objective function
-    objective = self.create_objective(y_true, logits)
+    # print('y_true: ', y_true)
+    # print('logits: ', logits)
+    objective = create_temperature_scaling_objective(y_true, logits)
+    # import pickle
+    # pickle.dump((y_true.numpy(), logits.numpy()), open('ts.pkl', 'wb'))
     #
     # Run the optimisation
-    results = tfp.optimizer.bfgs_minimize(objective, self.temperature, max_iterations=50)
-    # print(dir(results))
+    results = tfp.optimizer.lbfgs_minimize(objective, self.temperature, max_iterations=50)
     #
     # Check the results
+    new_temperature = results.position
+    print('# iterations: ', results.num_iterations.numpy())
+    print('# evaluations: ', results.num_objective_evaluations.numpy())
+    print('Optimised temperature: {}'.format(new_temperature.numpy()))
     if not results.converged:
       import warnings
       warnings.warn('LBFGS did not converge')
       print(results)
     #
     # Update the temperature
-    new_temperature = results.position
-    print('Optimised temperature: {}'.format(new_temperature))
     self.temperature.assign(new_temperature)
     #
     # Check the calibration error after optimising
@@ -322,7 +370,7 @@ class TemperatureScaling(tfkl.Layer):
         import warnings
         warnings.warn('Temperature scaling increased the calibration error!')
         # raise ValueError('Temperature scaling increased the calibration error!')
-    except NameError:
+    except NameError:  # This will trigger if `ce` was not created above.
       import warnings
       warnings.warn('Could not calculate calibration error.')
     #

bdlb/diabetic_retinopathy_diagnosis/benchmark.py
@@ -105,7 +105,7 @@ class DiabeticRetinopathyDiagnosisBenchmark(Benchmark):
     fractions = np.asarray([0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
     # Metrics for evaluation
-    metrics = list(zip(["accuracy", "auc"], cls.metrics()))
+    metrics = list(zip(["accuracy", "AUROC", "AUPRC"], cls.metrics()))
     if additional_metrics is not None:
       metrics = metrics + additional_metrics
 
@@ -232,7 +232,9 @@ class DiabeticRetinopathyDiagnosisBenchmark(Benchmark):
     """Evaluation metrics used for monitoring training."""
     import tensorflow as tf
     tfk = tf.keras
-    return [tfk.metrics.BinaryAccuracy(), tfk.metrics.AUC()]
+    return [tfk.metrics.BinaryAccuracy(),
+            tfk.metrics.AUC(curve='ROC'),
+            tfk.metrics.AUC(curve='PR')]
 
   @staticmethod
   def class_weight():